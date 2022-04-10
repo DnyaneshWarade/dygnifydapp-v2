14c07fbcaa17aa543fd895d937eac65fe7922b5a
sudhanshu1309 <sudhanshutripathi008@gmail.com>
2022-04-10 23:05:16 +0530

    tinlake like UI created

--- frontend/src/Routes.js
+++ frontend/src/Routes.js
@@ -1,9 +1,10 @@
 import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Borrow from "./borrow/Borrow";
-import Transaction from "./components/transaction/Transaction";
 import Home from "./home/Home";
 import Token from "./home/Token";
+import Overview from "./tinlake/components/overview/Overview";
+import Test from "./tinlake/Test"
 
 const Routes = () => {
   return (
@@ -13,7 +14,8 @@ const Routes = () => {
         <Route exact path="/pool" component={Home} />
         <Route exact path="/borrow" component={Borrow} />
         <Route exact path="/token" component={Token} />
-        <Route exact path="/test" component={Transaction} />
+        <Route exact path="/test" component={Test} />
+        <Route exact path="/overview" component={Overview} />
       </Switch>
     </BrowserRouter>
   );
